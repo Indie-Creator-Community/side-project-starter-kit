0e9839b6465287ec8889f4ec83e6751b6bca09fd
Samuel Llibre-Pillco <dev@zyruks.com>
2024-09-21 15:49:21 -0400

    chore(prettier): 🔧 migrate prettier config to mjs and update formatting rules
    - Migrated `prettier.config.cjs` to `prettier.config.mjs` for modern module support.
    - Updated prettier configuration:
      - Increased `printWidth` to 120.
      - Added configuration for `prettier-plugin-jsdoc` to improve JSDoc handling.
      - Updated import order to better manage module imports.
      - Adjusted Tailwind and Sort Imports plugins configuration for improved sorting and consistency.
    - Retained the essential settings such as single quotes, trailing commas, and specific plugin usage.

---
+++ prettier.config.mjs
@@ -0,0 +1,56 @@
+/** @typedef {import('@ianvs/prettier-plugin-sort-imports').PluginConfig} SortImportsConfig */
+/** @typedef {import('prettier').Config} PrettierConfig */
+/** @typedef {{ tailwindConfig: string }} TailwindConfig */
+
+/** @type {PrettierConfig | SortImportsConfig | TailwindConfig} */
+const config = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  jsxSingleQuote: false,
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  plugins: ['@ianvs/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss', 'prettier-plugin-jsdoc'],
+
+  /** Tailwind Plugin Config. */
+  tailwindConfig: './tailwind.config',
+
+  /** JSdoc Plugin Config. */
+  jsdocPrintWidth: 80,
+  tsdoc: true,
+  jsdocDescriptionWithDot: true,
+  jsdocCapitalizeDescription: false,
+  jsdocCommentLineStrategy: 'keep',
+  jsdocSeparateReturnsFromParam: false,
+  jsdocSeparateTagGroups: false,
+  jsdocPreferCodeFences: false,
+  jsdocLineWrappingStyle: 'greedy',
+
+  /** Sort Imports Plugin Config. */
+  importOrder: [
+    '',
+    '^(expo(.*)$)|^(expo$)',
+    '^(next/(.*)$)|^(next$)',
+    '^(react/(.*)$)|^(react$)|^(react-native(.*)$)',
+    '^@acme/(.*)$',
+    '^~/components/(.*)$',
+    '^~/styles/(.*)$',
+    '^~/utils/(.*)$',
+    '^~/(.*)$',
+    '^[./]',
+    '<THIRD_PARTY_MODULES>',
+  ],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+};
+
+export default config;
+
+/**
+ * @reference
+ * https://github.com/tailwindlabs/prettier-plugin-tailwindcss
+ * https://www.npmjs.com/package/prettier-plugin-jsdoc
+ * https://www.npmjs.com/package/@ianvs/prettier-plugin-sort-imports
+ */
